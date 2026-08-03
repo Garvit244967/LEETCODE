7719f3d0a7bc9307a6126eefa33044f17e26d5ec
Garvit Batra <garvit.b0610@gmail.com>
2026-08-03 18:52:27 +0530

    Time: 1 ms (98.11%), Space: 43.2 MB (88%) - LeetHub

---
+++ 3110-score-of-a-string/3110-score-of-a-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        int length = s.length();
+        for(int i = 0; i < s.length()-1; i++){
+            sum += Math.abs(s.charAt(i) - s.charAt(i+1));
+        }
+        return sum;
+    }
+}
