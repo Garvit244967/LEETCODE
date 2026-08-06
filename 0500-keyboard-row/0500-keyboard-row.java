47eb1784221b38099876c4e0591d31aa437238bc
Garvit Batra <garvit.b0610@gmail.com>
2026-08-06 17:59:41 +0530

    Time: 1 ms (33.18%), Space: 43.1 MB (17.58%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String row1 = "qwertyuiop";
+        String row2 = "asdfghjkl";
+        String row3 = "zxcvbnm";
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(String word : words){
+            String s = word.toLowerCase();
+            String row;
+
+            if(row1.contains("" + s.charAt(0))){
+                row = row1;
+            }
+            else if(row2.contains("" + s.charAt(0))){
+                row = row2;
+            }
+            else{
+                row = row3;
+            }
+            boolean valid = true;
+            for(int i = 0; i < s.length(); i++){
+                if(!row.contains("" + s.charAt(i))){
+                    valid = false;
+                    break;
+                }
+            }
+            if(valid){
+                ans.add(word);
+            }
+        } 
+        return ans.toArray(new String[0]);
+    }
+}
