79b19e528ddd19af68312e4e02505d9b1d5bd44a
Garvit Batra <garvit.b0610@gmail.com>
2026-08-10 13:49:43 +0530

    Time: 0 ms (100%), Space: 26.4 MB (5.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -3,20 +3,18 @@ public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
         vector<int> zeroes;
-        vector<int> others;
+        vector<int> nonzero;
         vector<int> result;
-
         for(int i = 0; i < n; i++){
             if(nums[i] == 0){
                 zeroes.push_back(nums[i]);
             }
             else{
-                others.push_back(nums[i]);
+                nonzero.push_back(nums[i]);
             }
         }
-
-        for(int i = 0; i < others.size(); i++){
-            result.push_back(others[i]);
+        for(int i = 0; i < nonzero.size(); i++){
+            result.push_back(nonzero[i]);
         }
         for(int i = 0; i < zeroes.size(); i++){
             result.push_back(zeroes[i]);
