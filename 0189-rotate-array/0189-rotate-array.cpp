4c5aa07fc7b9fe93b3e9b29a156d078e7d259bfb
Garvit Batra <garvit.b0610@gmail.com>
2026-08-10 13:35:00 +0530

    Time: 0 ms (100%), Space: 31.1 MB (6.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -2,22 +2,14 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;
-        vector<int> withk;
-        vector<int> notk;
+        k  = k % n;
         vector<int> result;
 
-        for(int i = 0; i < n-k; i++){
-            notk.push_back(nums[i]);
-        }
         for(int i = n-k; i < n; i++){
-            withk.push_back(nums[i]);
+            result.push_back(nums[i]);
         }
-        for(int i = 0; i < withk.size(); i++){
-            result.push_back(withk[i]);
-        }
-        for(int i = 0; i < notk.size(); i++){
-            result.push_back(notk[i]);
+        for(int i = 0; i < n-k; i++){
+            result.push_back(nums[i]);
         }
         nums = result;
     }
