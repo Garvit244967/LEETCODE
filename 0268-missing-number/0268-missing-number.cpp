60b4e57510a059bce217283c9620bdc76ac2283d
Garvit Batra <garvit.b0610@gmail.com>
2026-08-10 16:02:24 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums){
+    int missingNumber(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
         for(int i = 1; i <= n; i++){
