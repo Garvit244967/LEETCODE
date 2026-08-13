73ba1d9285892d25724c94ef007dcea22ae42e20
Garvit Batra <garvit.b0610@gmail.com>
2026-08-13 18:04:03 +0530

    Time: 3 ms (28.48%), Space: 28.2 MB (42.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,6 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
         unordered_map<int,int> freq;
+
         for(int i = 0; i < n; i++){
             freq[nums[i]]++;
         }
