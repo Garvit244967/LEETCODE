547c5fb2399a5ca1f55bfac3af096c62c1d19ba9
Garvit Batra <garvit.b0610@gmail.com>
2026-08-18 19:03:50 +0530

    Time: 35 ms (86.51%), Space: 45.4 MB (55.65%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,13 +4,12 @@ public:
         int n = nums.size();
         unordered_map<int,int> map;
         map[0] = 1;
-
         int sum = 0;
         int count = 0;
-        for(int i = 0 ; i < n; i++){
+        for(int i = 0; i < n; i++){
             sum += nums[i];
-            if(map.find(sum - k) != map.end()){
-                count += map[sum - k];
+            if(map.find(sum-k) != map.end()){
+                count += map[sum-k];
             }
             map[sum]++;
         }
