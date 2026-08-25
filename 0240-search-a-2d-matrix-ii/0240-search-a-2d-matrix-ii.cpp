2ba945745cfb43a6ee08bba0463d87c1fd69e357
Garvit Batra <garvit.b0610@gmail.com>
2026-08-25 12:15:08 +0530

    Time: 56 ms (44.87%), Space: 18.7 MB (69.1%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -5,17 +5,17 @@ public:
         int m = matrix[0].size();
 
         int row = 0;
-        int col = m - 1;
+        int col = m-1;
 
         while(row < n && col >= 0){
             if(matrix[row][col] == target){
                 return true;
             }
-            else if(matrix[row][col] > target){
-                col--;
+            else if(matrix[row][col] < target){
+                row++;
             }
             else{
-                row++;
+                col--;
             }
         }
         return false;
