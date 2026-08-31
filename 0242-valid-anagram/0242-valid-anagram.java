903c0cf91df0211767a01155049f748e97fcbf82
Garvit Batra <garvit.b0610@gmail.com>
2026-08-31 23:06:13 +0530

    Time: 4 ms (83.44%), Space: 46.2 MB (47.31%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,7 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // for(int i = 0; i < s.length(); i++){
-        //     for(int j = 0; j < t.length(); j++){
-        //         if(s.charAt(i) == t.charAt(j)){
-        //             return true;
-        //         }
-        //     }
-        // }
-        // return false;
+        if(s.length() != t.length()) return false;
 
-        if(s.length() != t.length()){
-            return false;
-        }
         char[] a = s.toCharArray();
         char[] b = t.toCharArray();
 
